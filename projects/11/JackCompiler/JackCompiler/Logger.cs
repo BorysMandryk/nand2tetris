bed55c7821c967f5d7b3cc09243ec568a704bf4d
BorysMandryk <borys.mandryk@gmail.com>
2023-12-21 15:40:05 +0200

    Add scripts from JackAnalyzer

---
+++ projects/11/JackCompiler/JackCompiler/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace JackCompiler
+{
+    internal class Logger : IDisposable
+    {
+        protected TextWriter _writer;
+        protected bool _disposed = false;
+        public bool Show { get; set; } = true;
+
+        public Logger(TextWriter output)
+        {
+            _writer = output;
+        }
+
+        public virtual void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _writer.Dispose();
+            _disposed = true;
+        }
+
+        public void Log(string str)
+        {
+            if(Show)
+            {
+                _writer.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ projects/11/JackCompiler/JackCompiler/LoggerTokenizer.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace JackCompiler
+{
+    internal class LoggerTokenizer : Logger
+    {
+        private JackTokenizer _tokenizer;
+
+        public LoggerTokenizer(TextWriter output, JackTokenizer tokenizer)
+            : base(output)
+        {
+            _tokenizer = tokenizer;
+        }
+
+        public void Log()
+        {
+            Log($"{_tokenizer.Filename} " +
+                $"({_tokenizer.CurrentLine}, {_tokenizer.CurrentSymbol}): " +
+                $"Unexpected token type {_tokenizer.GetTokenType()} ");
+        }
+    }
+}
