1c201b18a95e40c7e5fac046c8df61cbd67478de
BorysMandryk <borys.mandryk@gmail.com>
2023-12-22 20:59:12 +0200

    Implement functionality of symbol tables

--- projects/11/JackCompiler/JackCompiler/SymbolTable.cs
+++ projects/11/JackCompiler/JackCompiler/SymbolTable.cs
@@ -1,37 +1,66 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace JackCompiler
 {
     internal class SymbolTable
     {
+        private Dictionary<string, SymbolTableItem> _table;
+
         public SymbolTable()
         {
-            throw new NotImplementedException();
+            _table = new Dictionary<string, SymbolTableItem>();
         }
 
-        public void StartSubroutine()
+        public void Define(string name, VariableKind kind, string type, int index)
         {
-            throw new NotImplementedException();
+            _table[name] = new SymbolTableItem(kind, type, index);
         }
 
-        public int VarCount()
+        public int VarCount(VariableKind kind)
         {
-            throw new NotImplementedException();
+            return _table.Count(i => i.Value.Kind == kind);
         }
 
         public VariableKind KindOf(string name)
         {
-            throw new NotImplementedException();
+            if(_table.TryGetValue(name, out SymbolTableItem item))
+            {
+                return item.Kind;
+            }
+            return VariableKind.NONE;
         }
 
         public string TypeOf(string name)
         {
-            throw new NotImplementedException();
+            return _table[name].Type;
         }
 
-        public string IndexOf(string name)
+        public int IndexOf(string name)
         {
-            throw new NotImplementedException();
+            return _table[name].Index;
+        }
+
+        public bool TryTypeOf(string name, out string type)
+        {
+            if (_table.TryGetValue(name, out SymbolTableItem item))
+            {
+                type =  item.Type;
+                return true;
+            }
+            type = default;
+            return false;
+        }
+
+        public bool TryIndexOf(string name, out int index)
+        {
+            if (_table.TryGetValue(name, out SymbolTableItem item))
+            {
+                index = item.Index;
+                return true;
+            }
+            index = default;
+            return false;
         }
     }
 }
---
+++ projects/11/JackCompiler/JackCompiler/SymbolTableItem.cs
@@ -0,0 +1,16 @@
+﻿namespace JackCompiler
+{
+    internal class SymbolTableItem
+    {
+        public VariableKind Kind { get; set; }
+        public string Type { get; set; }
+        public int Index { get; set; }
+
+        public SymbolTableItem(VariableKind kind, string type, int index)
+        {
+            Kind = kind;
+            Type = type;
+            Index = index;
+        }
+    }
+}
