6ce5fafd7d71a194048fd55113c5e4a682012d9f
BorysMandryk <borys.mandryk@gmail.com>
2023-12-23 07:55:23 +0200

    Implement VMWriter functionality

--- projects/11/JackCompiler/JackCompiler/VMWriter.cs
+++ projects/11/JackCompiler/JackCompiler/VMWriter.cs
@@ -1,62 +1,104 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace JackCompiler
 {
-    internal class VMWriter
+    internal class VMWriter : IDisposable
     {
+        private static readonly Dictionary<MemorySegment, string> _memorySegmentToString = new Dictionary<MemorySegment, string>()
+        {
+            { MemorySegment.CONST, "constant" },
+            { MemorySegment.STATIC, "static" },
+            { MemorySegment.LOCAL, "local" },
+            { MemorySegment.ARG, "argument" },
+            { MemorySegment.THIS, "this" },
+            { MemorySegment.THAT, "that" },
+            { MemorySegment.TEMP, "temp" },
+            { MemorySegment.POINTER, "pointer" }
+        };
+
+        private static readonly Dictionary<ArithmeticCommand, string> _arithmeticToString = new Dictionary<ArithmeticCommand, string>()
+        { 
+            {ArithmeticCommand.ADD, "add" },
+            {ArithmeticCommand.SUB, "sub" },
+            {ArithmeticCommand.NEG, "neg" },
+            {ArithmeticCommand.EQ, "eq" },
+            {ArithmeticCommand.GT, "gt" },
+            {ArithmeticCommand.LT, "lt" },
+            {ArithmeticCommand.AND, "and" },
+            {ArithmeticCommand.OR, "or" },
+            {ArithmeticCommand.NOT, "not" }
+        };
+
+        private StreamWriter _streamWriter;
+
+        private bool _disposed = false;
+
         public VMWriter(string output)
         {
-            throw new NotImplementedException();
+            _streamWriter = new StreamWriter(output);
+        }
+
+        public virtual void Dispose()
+        {
+            if(_disposed)
+            {
+                return;
+            }
+
+            _streamWriter.Dispose();
+            _disposed = true;
         }
 
         public void WritePush(MemorySegment segment, int index)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"push {_memorySegmentToString[segment]} {index}");
         }
 
         public void WritePop(MemorySegment segment, int index)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"pop {_memorySegmentToString[segment]} {index}");
         }
 
         public void WriteArithmetic(ArithmeticCommand command)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine(_arithmeticToString[command]);
         }
 
         public void WriteLabel(string label)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"label {label}");
         }
 
         public void WriteGoto(string label)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"goto {label}");
         }
 
         public void WriteIf(string label)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"if-goto {label}");
         }
 
         public void WriteCall(string name, int nArgs)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"call {name} {nArgs}");
         }
 
         public void WriteFunction(string name, int nLocals)
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"function {name} {nLocals}");
         }
 
         public void WriteReturn()
         {
-            throw new NotImplementedException();
+            _streamWriter.WriteLine($"return");
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            Dispose();
         }
     }
 }
